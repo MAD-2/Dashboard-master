d6262c4f7ce847e97918ba234e473824bb884fb1
MAD-2 <mmuradiabdullah88@gmail.com>
2022-01-24 04:06:22 +0300

    ..

--- js/charts.js
+++ js/charts.js
@@ -37,6 +37,17 @@ const myChart = new Chart(ctx, {
 
 // Line Chart for Orders
 var ordersChart = document.getElementById("myAreaChart");
+const labels = Utils.months({count: 7});
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+};
 
 // Bar Chart for Visitor
 var visitorChart = document.getElementById("myBarChart");
